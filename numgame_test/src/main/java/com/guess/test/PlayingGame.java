01086e702722b42ea8afb3a7db279c419866b6e7
= <=>
2013-04-08 18:38:30 +0800

    设定游戏次数，输出“游戏结束”

--- numgame_test/src/main/java/com/guess/test/PlayingGame.java
+++ numgame_test/src/main/java/com/guess/test/PlayingGame.java
@@ -5,17 +5,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.lang.System;
 
 public class PlayingGame
 {
     public static void main(String args[]) throws IOException
     {
-
+      for(int i=0;i<4;i++)
+      {
         System.out.println("请输入一个四位数：");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Integer input_num = Integer.parseInt(reader.readLine());
         NumGame numGame=new NumGame();
         System.out.println(numGame.get_result_of_comparing_two_numbers(input_num,numGame.create_four_digits_random_num()));
+      }
     }
+    System.out.println("游戏结束");
 }
 
