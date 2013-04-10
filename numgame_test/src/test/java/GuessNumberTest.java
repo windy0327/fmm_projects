f62315555999e7d0157751667c8aa69fb976be02
= <=>
2013-04-10 15:04:29 +0800

    重构代码

--- numgame_test/src/test/java/GuessNumberTest.java
+++ numgame_test/src/test/java/GuessNumberTest.java
@@ -13,13 +13,6 @@ import static org.hamcrest.core.Is.is;
 public class GuessNumberTest {
 
     @Test
-
-    public void should_remove_subscriber_when_close()  {
-
-        assertThat(MyMath.add(2, 3), is(5));
-    }
-
-    @Test
     public  void should_be_four_digits_num(){
         Integer m=2356;
         assertThat(NumGame.create_four_digits_random_num().toString().length(),is(4));
@@ -35,22 +28,27 @@ public class GuessNumberTest {
     public void should_be_1A0B_when_input_having_one_equal_digit_numbers(){
         assertThat(NumGame.get_result_of_comparing_two_numbers(2456,2980),is("1A0B"));
     }
+
     @Test
     public void output_0A4B(){
         assertThat(NumGame.get_result_of_comparing_two_numbers(1234,4321),is("0A4B"));
     }
+
     @Test
     public void output_2A2B(){
         assertThat(NumGame.get_result_of_comparing_two_numbers(1245,1254),is("2A2B"));
     }
+
     @Test
     public void output_0A0B(){
         assertThat(NumGame.get_result_of_comparing_two_numbers(2345,6789),is("0A0B"));
     }
+
     @Test
     public void output_1A3B(){
         assertThat(NumGame.get_result_of_comparing_two_numbers(5555,5432),is("1A3B"));
     }
+
     @Test
     public void output_4A12B(){
         assertThat(NumGame.get_result_of_comparing_two_numbers(6666,6666),is("4A12B"));
