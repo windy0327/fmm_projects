c4f0a89823740e6db0e3859c7100467d8ad9f452
= <=>
2013-04-10 14:45:17 +0800

    增加了功能：在一次游戏中产生的随机数保持不变

--- numgame_test/src/main/java/com/guess/test/PlayingGame.java
+++ numgame_test/src/main/java/com/guess/test/PlayingGame.java
@@ -11,13 +11,15 @@ public class PlayingGame
 {
     public static void main(String args[]) throws IOException
     {
+      NumGame numGame=new NumGame();
+      int produced_four_digits_num= numGame.create_four_digits_random_num();
       for(int i=0;i<4;i++)
       {
         System.out.println("请输入一个四位数：");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Integer input_num = Integer.parseInt(reader.readLine());
         NumGame numGame=new NumGame();
-        System.out.println(numGame.get_result_of_comparing_two_numbers(input_num,numGame.create_four_digits_random_num()));
+        System.out.println(numGame.get_result_of_comparing_two_numbers(input_num,produced_four_digits_num));
       }
     }
     System.out.println("游戏结束");
