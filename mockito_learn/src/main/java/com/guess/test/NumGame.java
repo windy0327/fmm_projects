51305886ae804c375a5eddf27e2f6b989d9c6d42
= <=>
2013-04-16 17:22:04 +0800

    format code

--- mockito_learn/src/main/java/com/guess/test/NumGame.java
+++ mockito_learn/src/main/java/com/guess/test/NumGame.java
@@ -2,44 +2,35 @@ package com.guess.test;
 
 import java.io.*;
 
-public class NumGame
-{
-    public static Integer randomNum=0;
+public class NumGame {
+    public static Integer randomNum = 0;
 
 
-    public static void getRandomNum()
-    {
-        randomNum = (int) (Math.random()*9000+1000);
+    public static void getRandomNum() {
+        randomNum = (int) (Math.random() * 9000 + 1000);
     }
 
-    public static String getGameResult(Integer inputNum,Integer randomNum)
-    {
-        int how_many_A=0;
-        int how_many_B=0;
+    public static String getGameResult(Integer inputNum, Integer randomNum) {
+        int how_many_A = 0;
+        int how_many_B = 0;
 
-        for(int i=0;i<4;i++)
-        {
-            for(int j=0;j<4;j++)
-            {
-                if(inputNum.toString().charAt(i)== randomNum.toString().charAt(j)&&i==j)
-                {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (inputNum.toString().charAt(i) == randomNum.toString().charAt(j) && i == j) {
                     how_many_A++;
                 }
-                if(inputNum.toString().charAt(i)== randomNum.toString().charAt(j)&&i!=j)
-                {
+                if (inputNum.toString().charAt(i) == randomNum.toString().charAt(j) && i != j) {
                     how_many_B++;
                 }
             }
         }
 
-        return how_many_A+"A"+how_many_B+"B";
+        return how_many_A + "A" + how_many_B + "B";
     }
 
-    public static Integer inputNum(BufferedReader reader)
-    {
+    public static Integer inputNum(BufferedReader reader) {
         Integer inputNum = null;
-        try
-        {
+        try {
             inputNum = Integer.parseInt(reader.readLine());
         } catch (IOException e) {
             e.printStackTrace();
@@ -48,44 +39,33 @@ public class NumGame
         return inputNum;
     }
 
-    public static void playGame(Integer num, BufferedReader reader, PrintStream mockOut)
-    {
+    public static void playGame(Integer num, BufferedReader reader, PrintStream mockOut) {
 
-        for(int i=0;i<6;i++)
-        {
+        for (int i = 0; i < 6; i++) {
             mockOut.println("请输入一个四位数：");
 
             Integer inputNum = inputNum(reader);
-
             mockOut.println(NumGame.getGameResult(inputNum, num));
 
-            if(inputNum.intValue()==num.intValue())
-           {
-              mockOut.println("Congratulations!");
-              break;
-           }
+            if (inputNum.intValue() == num.intValue()) {
+                mockOut.println("Congratulations!");
+                break;
+            }
         }
 
         mockOut.println("Game over!");
     }
 
-    public static void makeDigitsDifferent()
-    {
+    public static void makeDigitsDifferent() {
         NumGame.getRandomNum();
 
-        String randomNumStr=randomNum.toString();
-
-        int j=0;
-        boolean isDifferent=false;
-
-        for(int i=0;!isDifferent;i++)
-        {
-            if(randomNumStr.charAt(j)!=randomNumStr.charAt(j+1)&&randomNumStr.charAt(j+1)!=randomNumStr.charAt(j+2)&&randomNumStr.charAt(j+2)!=randomNumStr.charAt(j+3))
-            {
-                isDifferent=true;
-            }
-            else
-            {
+        int j = 0;
+        boolean isDifferent = false;
+        String randomNumStr = randomNum.toString();
+        for (int i = 0; !isDifferent; i++) {
+            if (randomNumStr.charAt(j) != randomNumStr.charAt(j + 1) && randomNumStr.charAt(j + 1) != randomNumStr.charAt(j + 2) && randomNumStr.charAt(j + 2) != randomNumStr.charAt(j + 3)) {
+                isDifferent = true;
+            } else {
                 NumGame.getRandomNum();
             }
         }
--- mockito_learn/src/test/java/GuessNumberTest.java
+++ mockito_learn/src/test/java/GuessNumberTest.java
@@ -1,4 +1,3 @@
-
 import com.guess.test.NumGame;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,84 +20,94 @@ public class GuessNumberTest {
 
     @Test
     public void should_be_num_when_input() throws IOException {
-        BufferedReader reader= Mockito.mock(BufferedReader.class);
+        BufferedReader reader = Mockito.mock(BufferedReader.class);
         when(reader.readLine()).thenReturn("3");
+
         assertThat(NumGame.inputNum(reader), is(3));
     }
 
     @Test
-    public void should_be_four_digits_num(){
+    public void should_be_four_digits_num() {
         NumGame.getRandomNum();
-        assertThat(NumGame.randomNum.toString().length(),is(4));
+
+        assertThat(NumGame.randomNum.toString().length(), is(4));
     }
 
     @Test
-    public void should_have_four_different_digits(){
-        int i=0;
+    public void should_have_four_different_digits() {
+        int i = 0;
+
         NumGame.makeDigitsDifferent();
-        assertThat(NumGame.randomNum.toString().charAt(i)!=NumGame.randomNum.toString().charAt(i+1),is(true));
-        assertThat(NumGame.randomNum.toString().charAt(i+1)!=NumGame.randomNum.toString().charAt(i+2),is(true));
-        assertThat(NumGame.randomNum.toString().charAt(i+2)!=NumGame.randomNum.toString().charAt(i+3),is(true));
+
+        assertThat(NumGame.randomNum.toString().charAt(i) != NumGame.randomNum.toString().charAt(i + 1), is(true));
+        assertThat(NumGame.randomNum.toString().charAt(i + 1) != NumGame.randomNum.toString().charAt(i + 2), is(true));
+        assertThat(NumGame.randomNum.toString().charAt(i + 2) != NumGame.randomNum.toString().charAt(i + 3), is(true));
     }
 
     @Test
-    public void should_be_4A0B_when_input_equal_numbers(){
-        assertThat(NumGame.getGameResult(2345,2345),is("4A0B"));
+    public void should_be_4A0B_when_input_equal_numbers() {
+        assertThat(NumGame.getGameResult(2345, 2345), is("4A0B"));
     }
 
     @Test
-    public void should_be_1A0B_when_input_having_one_equal_digit_numbers(){
-        assertThat(NumGame.getGameResult(2456,2980),is("1A0B"));
+    public void should_be_1A0B_when_input_having_one_equal_digit_numbers() {
+        assertThat(NumGame.getGameResult(2456, 2980), is("1A0B"));
     }
 
     @Test
-    public void output_0A4B(){
-        assertThat(NumGame.getGameResult(1234,4321),is("0A4B"));
+    public void output_0A4B() {
+        assertThat(NumGame.getGameResult(1234, 4321), is("0A4B"));
     }
 
     @Test
-    public void output_2A2B(){
-        assertThat(NumGame.getGameResult(1245,1254),is("2A2B"));
+    public void output_2A2B() {
+        assertThat(NumGame.getGameResult(1245, 1254), is("2A2B"));
     }
 
     @Test
-    public void output_0A0B(){
-        assertThat(NumGame.getGameResult(2345,6789),is("0A0B"));
+    public void output_0A0B() {
+        assertThat(NumGame.getGameResult(2345, 6789), is("0A0B"));
     }
 
     @Test
-    public void output_1A3B(){
-        assertThat(NumGame.getGameResult(5555,5432),is("1A3B"));
+    public void output_1A3B() {
+        assertThat(NumGame.getGameResult(5555, 5432), is("1A3B"));
     }
 
     @Test
     public void should_print_right_when_input_equal_numbers() throws IOException {
-        BufferedReader reader=Mockito.mock(BufferedReader.class);
-        PrintStream  mockOut=Mockito.mock(PrintStream.class);
+        BufferedReader reader = Mockito.mock(BufferedReader.class);
+        PrintStream mockOut = Mockito.mock(PrintStream.class);
+
         when(reader.readLine()).thenReturn("6789");
         playGame(6789, reader, mockOut);
-        verify(mockOut,times(1)).println("请输入一个四位数：");
-        verify(mockOut,times(1)).println("4A0B");
-        verify(mockOut,times(1)).println("Congratulations!");
-        verify(mockOut,times(1)).println("Game over!");
+
+        verify(mockOut, times(1)).println("请输入一个四位数：");
+        verify(mockOut, times(1)).println("4A0B");
+        verify(mockOut, times(1)).println("Congratulations!");
+        verify(mockOut, times(1)).println("Game over!");
     }
 
     @Test
     public void should_print_1A0B_6_times() throws IOException {
-        BufferedReader reader=Mockito.mock(BufferedReader.class);
-        PrintStream  mockOut=Mockito.mock(PrintStream.class);
+        BufferedReader reader = Mockito.mock(BufferedReader.class);
+        PrintStream mockOut = Mockito.mock(PrintStream.class);
+
         when(reader.readLine()).thenReturn("6789");
         playGame(6123, reader, mockOut);
-        verify(mockOut,times(6)).println("1A0B");
+
+        verify(mockOut, times(6)).println("1A0B");
     }
 
     @Test
     public void should_print_Game_over() throws IOException {
-        BufferedReader reader=Mockito.mock(BufferedReader.class);
-        PrintStream mockOut=Mockito.mock(PrintStream.class);
+        BufferedReader reader = Mockito.mock(BufferedReader.class);
+        PrintStream mockOut = Mockito.mock(PrintStream.class);
+
         when(reader.readLine()).thenReturn("1234");
         playGame(1567, reader, mockOut);
-        verify(mockOut,times(1)).println("Game over!");
+
+        verify(mockOut, times(1)).println("Game over!");
     }
 
 }
